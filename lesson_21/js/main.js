1ff5d78853d6e72937929085a4d5be5673db2d40
Illia <119691181+illiad24@users.noreply.github.com>
2023-04-12 17:29:48 +0300

    Add files via upload

---
+++ lesson_21/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("click", function (e) {
+    const targetElement = e.target;
+    if (targetElement.closest('.icon-menu')) {
+        document.documentElement.classList.toggle('menu-open');
+        e.preventDefault();
+    }
+})
