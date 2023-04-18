09eff9ee196c0c52399a378bad0d21e4e1aad6a4
Illia <119691181+illiad24@users.noreply.github.com>
2023-04-18 14:20:07 +0300

    Delete lesson_21 directory

--- lesson_21/js/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("click", function (e) {
-    const targetElement = e.target;
-    if (targetElement.closest('.icon-menu')) {
-        document.documentElement.classList.toggle('menu-open');
-        e.preventDefault();
-    }
-})
